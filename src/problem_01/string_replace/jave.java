7e6200e10fb772bf407e1df0d0f94bc024b9dd28
shoreful <shorefulislam01732254306@gmail.com>
2023-07-20 06:11:16 +0600

    replace

---
+++ src/problem_01/string_replace/jave.java
@@ -0,0 +1,27 @@
+package problem_01.string_replace;
+
+public class jave {
+    private String stateName;
+    public string_replace(){
+        stateName="Mississippi";
+    }
+    public string_replace(String statename){
+        this.stateName=statename;
+    }
+    public String replaceI(){
+        String NewStateName = stateName.replace("i","ii");
+        return NewStateName;
+    }
+    public String replaceI(String statename){
+        String NewStateName = statename.replace("i","ii");
+        return NewStateName;
+    }
+    public String replaceSS(){
+        String NewStateName = stateName.replace("ss","s");
+        return NewStateName;
+    }
+    public String replaceSS(String statename){
+        String NewStateName = statename.replace("ss","s");
+        return NewStateName;
+    }
+}
